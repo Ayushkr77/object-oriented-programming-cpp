eb850c417914328309c1b29d3d674778260cc127
Ayush Kumar Sinha <ayushkumar5990@gmail.com>
2026-07-11 01:43:15 +0530

    v3: Moved member function definitions outside the class using Scope Resolution Operator (::)

--- OOP Foundations + Classes/StudentManagement.cpp
+++ OOP Foundations + Classes/StudentManagement.cpp
@@ -1,3 +1,4 @@
+// note that in this file, many commits will be there, those are versions which represents the learning prgress how we learnt each thing one by one. try to see all the commits of this file
 #include<bits/stdc++.h>
 using namespace std;
 class Student {
@@ -5,21 +6,25 @@ public:   // bcz default is private
     string name;
     int age;
     
-    void display() {
-        cout<<"Name: "<<name<<endl;
-        cout<<"Age: "<<age<<endl;
-    }
-    void input() {
-        cin>>name>>age;
-    }
+    void display();
+    void input();
 };
 
+// we cannot define below functions inside int main. Function definitions cannot be inside another function. Remember: A function cannot be defined inside another function in C++. Only function calls are allowed inside main().
+void Student:: display() {   // compiler internally behaves like void Student::display(Student* this)
+    cout<<"Name: "<<name<<endl;
+    cout<<"Age: "<<age<<endl;
+}
+void Student::input() {
+    cin>>name>>age;
+}
+
+
 int main() {
+
     Student s1,s2,s3;
-    
+
     s1.input();
     
     s1.display();
-    s2.display();
-    s3.display();
 }
