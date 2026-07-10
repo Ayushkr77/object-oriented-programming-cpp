9da318c366b2f4a2f51b14148a22b3ad620b3243
Ayush Kumar Sinha <ayushkumar5990@gmail.com>
2026-07-11 02:15:34 +0530

    v6: Added getters, setters, and input validation

--- OOP Foundations + Classes/StudentManagement.cpp
+++ OOP Foundations + Classes/StudentManagement.cpp
@@ -9,20 +9,60 @@ private:   // default is private as well
 public:  // need to keep this public so that we can access this from outside the class
     void display();
     void input();
+
+    // setters and getters(bcz since the data members are private, we need to make functions to update them)
+    void setName(string newName);
+    void setAge(int newAge);
+    string getName();
+    int getAge();
 };
 
 // we cannot define below functions inside int main. Function definitions cannot be inside another function. Remember: A function cannot be defined inside another function in C++. Only function calls are allowed inside main().
 void Student:: display() {   // compiler internally behaves like void Student::display(Student* this)
     cout << "----------------------\n";
-    cout << "Name : " << name << endl;
-    cout << "Age  : " << age << endl;
+    cout << "Name : " << getName() << endl;  // name will also work instead of getName() but for now its kept intentionally as we are learning setters and getters
+    cout << "Age  : " << getAge() << endl;
     cout << "----------------------\n";
 }
 void Student::input() {
+    string tempName;
+    int tempAge;
     cout << "Enter Name : ";
-    cin >> name;
+    cin >> tempName;
     cout << "Enter Age  : ";
-    cin >> age;
+    cin >> tempAge;
+    // Use setters so validation is applied
+    setName(tempName);
+    setAge(tempAge);
+
+}
+
+
+void Student::setName(string newName) {
+    if (!newName.empty()) {
+        name = newName;
+    }
+    else {
+        cout << "Invalid Name!\n";
+    }
+
+}
+
+void Student::setAge(int newAge) {
+    if (newAge >= 0) {
+        age = newAge;
+    }
+    else {
+        cout << "Invalid Age!\n";
+    }
+}
+
+string Student::getName() {
+    return name;
+}
+
+int Student::getAge() {
+    return age;
 }
 
 
@@ -34,11 +74,26 @@ int main() {
     s2.input();
     s3.input();
 
-    cout << "\nBefore Modification\n";
+    cout << "\nStudent Details\n\n";
 
     s1.display();
     s2.display();
     s3.display();
+
+    // Direct access is not allowed
+    // s1.age = 25;      // Error
+    // s1.name = "Ayush"; // Error
+
+    // Updating using setters
+    s2.setName("Rohit");
+    s2.setAge(40);
+
+    cout << "\nAfter Updating Student 2\n\n";
+
+    s1.display();
+    s2.display();
+    s3.display();
+
 
     // this will not work as the variables/ data members are private 
     // s2.age = 40; 
