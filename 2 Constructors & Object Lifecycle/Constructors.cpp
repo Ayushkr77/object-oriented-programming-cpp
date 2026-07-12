de34a5db4ea6e42318b8c4e227ba996923ce3e12
Ayush Kumar Sinha <ayushkumar5990@gmail.com>
2026-07-12 19:09:29 +0530

    v1: Added user-defined default constructor and demonstrated automatic object initialization

---
+++ 2 Constructors & Object Lifecycle/Constructors.cpp
@@ -0,0 +1,75 @@
+// note that in this file, many commits will be there, those are versions which represents the learning prgress how we learnt each thing one by one. try to see all the commits of this file. and see each and every block, every comment carefully on each version as comments as well changes
+// this file is the continuation of StudentManagement.cpp file(1 OOP Foundations + Classes\StudentManagement.cpp)
+
+// ============================================================================
+// Constructors.cpp
+//
+// This file contains the complete implementation of Day 2 concepts.
+// Each Git commit represents one version, gradually introducing constructors,
+// object lifecycle, dynamic memory management, copy constructors,
+// shallow/deep copy, and the Rule of Three.
+//
+// Explore the commit history to see how the code evolves step by step.
+// ============================================================================
+
+#include<bits/stdc++.h>
+using namespace std;
+class Student {
+private:   // default is private as well
+    string name;
+    int age;
+
+    static int studentCount;  // // Shared by all Student objects
+    
+public:  // need to keep this public so that we can access this from outside the class
+    // Member Function Definition
+    void display();
+
+    
+    static void displayStudentCount(); // Static Member Functions
+
+
+    Student() {  // User-defined Default Constructor
+        name = "Unknown";
+        age = 0;
+        studentCount++;
+        cout << "Default Constructor Called\n";
+    }
+
+};
+
+
+void Student::displayStudentCount() {
+    cout << "\nTotal Students Created : " << studentCount << endl;
+}
+
+
+// Static Data Member Initialization
+int Student::studentCount = 0;
+
+// we cannot define below functions inside int main. Function definitions cannot be inside another function. Remember: A function cannot be defined inside another function in C++. Only function calls are allowed inside main()// Member functions cannot be defined inside another function. They must be defined either inside the class or outside using ::..
+
+void Student:: display() {   // compiler internally behaves like void Student::display(Student* this)
+    cout << "----------------------\n";
+    cout << "Name : " << name << endl;  // The compiler automatically treats it as: cout << this->name;
+    cout << "Age  : " << age << endl;
+    cout << "----------------------\n";
+}
+
+
+
+
+
+int main() {
+    Student s1;
+    Student s2;
+    Student s3;
+
+    cout << "\nStudent Details\n\n";
+
+    s1.display();
+    s2.display();
+    s3.display();
+
+    Student::displayStudentCount();
+}
